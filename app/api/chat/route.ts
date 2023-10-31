ecce31564e33cc0e6e571a4b426d13ce014bd021
PlutoniaX <junmagic88@gmail.com>
2023-10-31 22:33:59 +1100

    initial commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,7 +8,8 @@ import { nanoid } from '@/lib/utils'
 export const runtime = 'edge'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
+  basePath: process.env.OPENAI_API_BASE
 })
 
 const openai = new OpenAIApi(configuration)
